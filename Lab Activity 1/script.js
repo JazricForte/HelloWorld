fc0b497c6dd95bf6b09066e50f54fdab0567ce9b
JazricForte <Jazricfortexx@gmail.com>
2024-09-09 13:00:38 +0800

    Lab activity 1

---
+++ Lab Activity 1/script.js
@@ -0,0 +1,6 @@
+let quiz1 = prompt("Enter quiz 1: ");
+let quiz2 = prompt("Enter quiz 2: ");
+let quiz3 = prompt("Enter quiz 3: ");
+let sum = parseInt(parseInt(quiz1) + parseInt(quiz2) + parseInt(quiz3)) / parseInt(3);
+
+console.log(`Quiz average: ${sum}`);
