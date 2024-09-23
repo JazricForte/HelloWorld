b2dd82127dcacefc30fdea581b4535feb8e916d6
JazricForte <Jazricfortexx@gmail.com>
2024-09-23 11:33:40 +0800

    Arrays

---
+++ something/script.js
@@ -0,0 +1,31 @@
+console.log("START");
+
+const numbers = [1,2,3,4,5];
+console.log(numbers);
+
+numbers.push(9);
+numbers.push(1);
+console.log(numbers);
+
+numbers.pop();
+console.log(numbers);
+
+numbers.push(69);
+console.log(numbers);
+
+// hold alt+shift and down to instantly type!!!!
+
+//shift = enqeue
+//unshift is to add to the front!!
+//shift deletes front!!
+console.log("SHIFT/ENQEUE");
+
+numbers.unshift(3);
+numbers.unshift(4);
+numbers.unshift(7);
+console.log(numbers);
+
+numbers.shift();
+numbers.shift();
+numbers.shift();
+console.log(numbers);
