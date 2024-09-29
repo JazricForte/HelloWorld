5381c8385209f61d5d4aa3cef137a43ff6ec23f3
Jazric Forte <jazricfortexx@gmail.com>
2024-09-29 21:47:36 +0800

    Added a function to input information

--- Midterm Activity 1/script.js
+++ Midterm Activity 1/script.js
@@ -1,28 +1,49 @@
+let studentName = " ";
+let studentCourse = " ";
+let studentAddress = " ";
+let studentGrade = " ";
+
+function AddStudent() {
+    studentName = prompt("Please enter name");
+    studentCourse = prompt("Please enter course");
+    studentAddress = prompt("Please enter address");
+    studentGrade = prompt("Please enter grade");
+}
+
+AddStudent();
+
 const student = [
     {
-        "name":prompt("Please enter name");
-        "course":prompt("Please enter course");
-        "address":prompt("Please enter name");
-        "grade":prompt("Please enter grade");
+        "name":studentName,
+        "course":studentCourse,
+        "address":studentAddress,
+        "grade":studentGrade
     }
 ]
+
+
+
+console.log(student);
 
 var container = document.getElementById('student');
 console.log(container);
 var output = "";
 
-student.map((student, index)) => {
+student.map((student, index) => {
     output += "<h1>";
     output += "Name: ";
     output += student.name;
     output += "<h1>";
     output += "<br>";
+    output += "<p>";
     output += "Course: ";
     output += student.course;
     output += " Address: ";
     output += student.address;
     output += " Grade: ";
     output += student.grade;
-}
+    output += "<p>";
+});
 console.log(output);
 container.innerHTML = output;
+
